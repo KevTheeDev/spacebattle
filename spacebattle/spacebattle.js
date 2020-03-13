73e1ddd97dc97072742a7e8ce9ad90e2b8c8c034
KevinJ.Regis <dev.regis1@gmail.com>
2020-03-12 23:40:05 -0500

    spacebattle/spacebattle.js update

---
+++ spacebattle/spacebattle.js
@@ -0,0 +1,27 @@
+// hull** is the same as hitpoints. If hull reaches `0` or less, the ship is    
+    // destroyed
+// firepower** is the amount of damage done to the **hull** of the target
+// accuracy** is the chance between 0 and 1
+console.log(`kat`);
+
+class USship {
+    constructor(hull = 20, firepower = 5, accuracy = .7){
+        this.accuracy =(Math.random() * (.6 - 1) + 1)
+    }
+}
+console.log(USship);
+
+
+class AlienShip{
+    constructor(hull = 3, firepower = 2, accuracy = .6){
+
+    }
+}
+
+
+// // const alien1 = new Alien();
+// // const alien2 = new Alien();
+// // const alien3 = new Alien();
+// // const alien4 = new Alien();
+// // const alien5 = new Alien();
+// // const alien6 = new Alien();
